5f2371f5cef4028676fda7ac383b8c5950871c77
Fly <flytheonlyone111@gmail.com>
2025-03-20 20:57:12 +0700

    Hive server

--- model/Train.py
+++ model/Train.py
@@ -1,4 +1,4 @@
 def train_with_model(data, model):
-    base = data.drop(columns=['Level'])
-    model.fit(base, data['Level'])
+    base = data.drop(columns=['level'])
+    model.fit(base, data['level'])
     return model
