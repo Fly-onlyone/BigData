a92491c7845f80846f30c2ce3e725a0c0171662c
Fly <flytheonlyone111@gmail.com>
2025-03-21 08:39:26 +0700

    First setup

--- ApplyModel.py
+++ ApplyModel.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def rm_main(model, data: pd.DataFrame):
-    base = data.drop(columns=['Level'])
+    base = data.drop(columns=['level'])
     data['Predicted'] = model.predict(base)
     data.rm_metadata['Predicted'] = (None, 'prediction')
     return data
--- model/Train.py
+++ model/Train.py
@@ -1,4 +1,12 @@
+from sklearn.preprocessing import LabelEncoder
+
 def train_with_model(data, model):
     base = data.drop(columns=['level'])
+
+    for col in base.columns:
+        if base[col].dtype == 'object':
+            le = LabelEncoder()
+            base[col] = le.fit_transform(base[col])
+
     model.fit(base, data['level'])
     return model
